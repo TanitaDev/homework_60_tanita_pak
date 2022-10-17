03a1790ca60ce08d9ea4898fdca7717d71818513
Tanita <tanita.pak@gmail.com>
2022-10-17 19:59:54 +0600

    переписала функцию просмотра всех товаров и одного товара на классовые представления

--- source/source/urls.py
+++ source/source/urls.py
@@ -20,9 +20,9 @@ from webapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view, name='index'),
-    path('products/<int:pk>', single_product_view, name='product_view'),
-    path('add/', product_add_view, name='add'),
+    path('', IndexView.as_view(), name='index'),
+    path('products/<int:pk>', ProductView.as_view(), name='product_view'),
+    path('add/', ProductCreate.as_view(), name='add'),
     path('<int:pk>/edit', product_edit_view, name='edit'),
     path('<int:pk>/delete', delete_product_view, name='delete'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,55 +1,29 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from webapp.models import Product
 
 
-class Index_view(ListView):
-    pass
+class IndexView(ListView):
+    template_name = "index.html"
+    context_object_name = "products"
+    model = Product
+    ordering = ["category", "name"]
 
 
-def index_view(request):
-    search_query = request.GET.get('search', '')
-
-    if search_query:
-        products = Product.objects.order_by('category', 'name').filter(name__icontains=search_query).exclude(remainder='0')
-        context = {
-            'products': products
-        }
-        return render(request, 'index.html', context)
-    else:
-        products = Product.objects.order_by("category", "name")
-        context = {
-            'products': products
-        }
-        return render(request, 'index.html', context)
+class ProductView(DetailView):
+    template_name = "product.html"
+    model = Product
 
 
-def single_product_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {
-        'product': product
-    }
-    return render(request, 'product.html', context)
+class ProductCreate(CreateView):
+    template_name = 'article/create.html'
+    model = Product
 
-
-def product_add_view(request):
-    products = Product.objects.all()
-    context = {
-        'products': products
-    }
-    if request.method == "GET":
-        return render(request, "add_product.html", context)
-    elif request.method == "POST":
-        name = request.POST.get("name")
-        description = request.POST.get("description")
-        image = request.POST.get("image")
-        category = request.POST.get("category")
-        remainder = request.POST.get("remainder")
-        price = request.POST.get("price")
-        Product.objects.create(name=name, description=description, image=image, category=category, remainder=remainder, price=price)
-        return redirect('/')
+    def get_redirect_url(self):
+        return reverse('product.html', kwargs={'pk': self.object.pk})
 
 
 def product_edit_view(request, pk):
